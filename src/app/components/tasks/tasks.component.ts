ab43622199f94a198e6c0229a78a259a7d3295d8
Hemant Makar <hemant.makar@gmail.com>
2024-11-03 21:22:46 +0530

    added new tasks flow and store data in local storage

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,19 +1,32 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { IUser } from '../../models/iuser.model';
-import { TaskComponent } from "./task/task.component";
+import { TaskComponent } from './task/task.component';
 import { TaskService } from '../../services/task.service';
+import { ITask } from '../../models/itask';
+import { EAddTaskDialogState } from '../../app.constants';
+import { NewTaskComponent } from './new-task/new-task.component';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent, AsyncPipe],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.scss'
+  styleUrl: './tasks.component.scss',
 })
 export class TasksComponent {
   public user = input.required<IUser>();
-  protected tasks = computed(() => {
-    return this.taskService.getTasks(this.user().id)
-  });
+  protected addTaskDialogState = EAddTaskDialogState.Close;
   constructor(private taskService: TaskService) {}
+
+  public get getUserTasks(): Array<ITask> {
+    return this.taskService.getTasks(this.user().id);
+  }
+
+  public onOpenAddTaskDialog(): void {
+    this.addTaskDialogState = EAddTaskDialogState.Open;
+  }
+  public onCloseAddTask(): void {
+    this.addTaskDialogState = EAddTaskDialogState.Close;
+  }
 }
